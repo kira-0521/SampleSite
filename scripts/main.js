eea2c3c331137a27013b798682d3c6bd46b05273
Kira Tanaka <kiratanaka0521@gmail.com>
2020-09-01 22:28:21 +0900

    complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hero = new HeroSlider('.swiper-container');
-    hero.start();
+        const main = new Main();
+    });
     
-    const cb = function (el, isIntersecting) {
-            if(isIntersecting) {
-                const ta = new TweenTextAnimation(el);
-                ta.animate();
-            }
+    class Main {
+        //initは初期化。イニシャライズの意味。初期化とはどこでもそのクラスを扱えるように用いる処理
+        constructor() {
+            this.header = document.querySelector('.header');
+            this.sides = document.querySelectorAll('.side');
+            this._observers = [];
+            this._init();
         }
 
-        const so = new ScrollObserver('.tween-animate-title', cb);
+        set observers(val) {
+            this._observers.push(val);
+        }
 
-        const _inviewAnimation = function(el, inview) {
+        get observers() {
+            return this._observers;
+        }
+
+        _init() {
+            new MobileMenu();
+            this.hero = new HeroSlider('.swiper-container');
+            Pace.on('done', this._paceDone.bind(this));
+        }
+
+        _paceDone() {
+            this._scrollInit();
+        }
+
+        _inviewAnimation(el, inview) {
             if(inview) {
                 el.classList.add('inview');
             }else {
@@ -19,11 +37,85 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
+        _navAnimation(el, inview) {
+            if(inview) {
+                this.header.classList.remove('triggered');
+            }else {
+                this.header.classList.add('triggered');
+            }
+        }
 
-        const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
-    });
+        _textAnimation(el, inview) {
+            if(inview) {
+                const ta = new TweenTextAnimation(el);
+                ta.animate();
+            }
+        }
+
+        _toggleSlideAnimation(el, inview) {
+            if(inview) {
+                this.hero.start();
+            } else {
+                this.hero.stop();
+            }
+        }
+
+        _sideAnimation(el, inview) {
+            if(inview) {
+                this.sides.forEach(side => side.classList.add('inview'));
+            }else {
+                this.sides.forEach(side => side.classList.remove('inview'));
+            }
+        }
+
+        _scrollInit() {
+            this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false});
+            this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
+            this.observers = new ScrollObserver('.appear', this._inviewAnimation);
+            this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation);
+            this.observers = new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false});
+            this.observers = new ScrollObserver('#main-content', this._sideAnimation.bind(this), {once: false, rootMargin: "-300px 0px"});
+        }
+    }
 
 
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     const main = new Main();
+    
+// hero.start();
+    
+    // const cb = function (el, isIntersecting) {
+    //         if(isIntersecting) {
+    //             const ta = new TweenTextAnimation(el);
+    //             ta.animate();
+    //         }
+    //     }
+
+        // const so = new ScrollObserver('.tween-animate-title', cb);
+
+        // const _inviewAnimation = function(el, inview) {
+        //     if(inview) {
+        //         el.classList.add('inview');
+        //     }else {
+        //         el.classList.remove('inview');
+        //     }
+        // }
+
+
+        // const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+
+        // const header = document.querySelector('.header');
+        // const _navAnimation = function(el, inview) {
+        //     if(inview) {
+        //         header.classList.remove('triggered');
+        //     }else {
+        //         header.classList.add('triggered');
+        //     }
+        // }
+
+
+        // const so3 = new ScrollObserver('.nav-trigger', _navAnimation, {once: false});
+    // });
 
 
 
